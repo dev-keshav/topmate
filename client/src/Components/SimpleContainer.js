3f285ce1b54cadb54a272c22fa4ddef1b381f69a
dev-keshav <keshavkumar9431@gmail.com>
2024-01-07 05:29:19 +0530

    added components in firstPage

--- client/src/Components/SimpleContainer.js
+++ client/src/Components/SimpleContainer.js
@@ -1,18 +1,147 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import { Typography } from '@mui/material';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Typography } from "@mui/material";
+import Arrow from "../img/arrow.png";
+
+const imgStyle = {
+  marginTop: "50px",
+  marginBottom: "50px",
+  transform: "rotate(90deg)",
+};
+
+const containerStyle = {
+  display: "flex",
+  textAlign: "center",
+  justifyContent: "center",
+  borderRadius: "15px",
+  // boxShadow: "0 0px 20px rgba(0, 0, 0, 0.5)",
+};
+
+const headingStyle = {
+  color: "#2A2C7C",
+};
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ display: 'flex', textAlign: 'center', justifyContent: 'center', bgcolor: '#B4B0D8', height: '20vh', width: '20vw', borderRadius: '15px' }}>
-        <Typography variant='h4' sx={{ color: '#2A2C7C'}} mt={2}>Foundation</Typography>
-        </Box>
-      </Container>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "20px",
+          paddingTop: "100px",
+        }}
+      >
+        <Typography
+          style={{
+            textAlign: "center",
+            marginBottom: "50px",
+            textDecoration: "underline",
+            fontFamily: 'cursive'
+          }}
+          variant="h4"
+          sx={headingStyle}
+          mt={2}
+        >
+          What Sets Us Apart
+        </Typography>
+        <Container maxWidth="sm" style={containerStyle}>
+          <Box
+            sx={{
+              ...containerStyle,
+              bgcolor: "#B4B0D8",
+              height: "28vh",
+              width: "100%", // Full width on small screens
+            }}
+          >
+            <div style={{ marginLeft: "40px", marginRight: "40px" }}>
+              <Typography variant="h5" sx={{ color: "#2A2C7C", fontWeight: 900 }} mt={2} m={2}>
+                Comprehensive Learning Paths
+              </Typography>
+              <Typography variant="p" sx={{headingStyle, fontFamily: 'cursive'}} mt={2}>
+                Dive into a curated collection of courses covering a wide array
+                of programming languages and frameworks. From foundational
+                languages like JavaScript and Java to cutting-edge technologies
+                like React, Next.js, Node.js, and more – we've got you covered.
+              </Typography>
+            </div>
+          </Box>
+        </Container>
+        <img src={Arrow} alt="Additional Section Image" style={imgStyle} />
+        <Container maxWidth="sm" style={containerStyle}>
+          <Box
+            sx={{
+              ...containerStyle,
+              bgcolor: "#DAB5D5",
+              height: "26vh",
+              width: "100%", // Full width on small screens
+            }}
+          >
+            <div style={{ marginLeft: "40px", marginRight: "40px" }}>
+              <Typography variant="h5" sx={{ color: "#892F92", fontWeight: 900 }} mt={2} m={2}>
+                Real-world Projects
+              </Typography>
+              <Typography variant="p" sx={{headingStyle, fontFamily: 'cursive'}} mt={2}>
+                Theory is essential, but hands-on experience is where true
+                mastery happens. Every learning path is complemented with
+                real-world projects that challenge you to apply your knowledge,
+                fostering a deeper understanding of the concepts.
+              </Typography>
+            </div>
+          </Box>
+        </Container>
+        <img src={Arrow} alt="Additional Section Image" style={imgStyle} />
+        <Container maxWidth="sm" style={containerStyle}>
+          <Box
+            sx={{
+              ...containerStyle,
+              bgcolor: "#FBCFD0",
+              height: "26vh",
+              width: "100%", // Full width on small screens
+            }}
+          >
+            <div style={{ marginLeft: "40px", marginRight: "40px" }}>
+              <Typography variant="h5" sx={{ color: "#E26362", fontWeight: 900 }} mt={2} m={2}>
+                Expert Instructors
+              </Typography>
+              <Typography variant="p" sx={{headingStyle, fontFamily: 'cursive'}} mt={2}>
+                Learn from industry experts and seasoned professionals who bring
+                real-world experience to the virtual classroom. Our instructors
+                are passionate about teaching and committed to your success.
+              </Typography>
+            </div>
+          </Box>
+        </Container>
+        <img src={Arrow} alt="Additional Section Image" style={imgStyle} />
+
+        <Container maxWidth="sm" style={containerStyle}>
+          <Box
+            sx={{
+              ...containerStyle,
+              bgcolor: "#82ddd7",
+              height: "26vh",
+              width: "100%", // Full width on small screens
+            }}
+          >
+            <div style={{ marginLeft: "40px", marginRight: "40px" }}>
+              <Typography variant="h5" sx={{ color: "#10a399", fontWeight: 900 }} mt={2} m={2}>
+                Interactive Coding Playground
+              </Typography>
+              <Typography variant="p" sx={{headingStyle, fontFamily: 'cursive'}} mt={2}>
+                Practice what you learn in our interactive coding playground.
+                Write, test, and debug code directly within the platform,
+                gaining practical skills that are essential in the software
+                development field.
+              </Typography>
+            </div>
+          </Box>
+        </Container>
+      </div>
     </React.Fragment>
   );
 }
--- client/src/Pages/Firstpage.js
+++ client/src/Pages/Firstpage.js
@@ -1,20 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SecondSectionImage from "../img/first.jpg";
+import React, { useState } from "react";
+import SecondSectionImage from "../img/first.jpeg";
 import SimpleContainer from "../Components/SimpleContainer";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import QuoteImage from "../img/quote.png";
+import PipCImage from "../img/pipC.png";
 
 const landingPageStyle = {
   display: "flex",
-  flexDirection: "column",
-  minHeight: "10vh", 
+  flexDirection: "row",
+  justifyContent: "space-between",
+  minHeight: "60vh",
   textAlign: "center",
   padding: "10px",
-  backgroundColor: "#673EE6",
-  color: "#fff",
+  backgroundColor: "#4DD5CD",
+  color: "#1C1F1F",
 };
+
+const DemoPaper = styled(Paper)(({ theme }) => ({
+  width: 420,
+  padding: theme.spacing(2),
+  ...theme.typography.body2,
+  textAlign: "center",
+  backgroundColor: "#F7F7F7",
+}));
 
 const headerStyle = {
   marginBottom: "20px",
+  fontFamily: "Cedarville Cursive",
+  fontSize: "30px",
+  transform: "rotate(-5deg)",
 };
 
 const buttonStyle = {
@@ -31,60 +48,147 @@ const buttonStyle = {
 const sectionContainerStyle = {
   display: "flex",
   justifyContent: "space-between",
-  width: "100%",
-  marginTop: "20px",
-};
-
-const sectionStyle = {
-  width: "48%",
-  padding: "20px",
-  backgroundColor: "#fff",
-  color: "#333",
+  width: "80%",
+  marginTop: "-130px",
+  zIndex: "3",
+  marginLeft: "120px",
+  boxShadow: "0px 0px 500px rgba(0, 0, 0, 0.5)",
+  backgroundColor: "#F7F7F7",
 };
 
 const imgStyle = {
-  width: "50%",
+  width: "70%",
   borderRadius: "8px",
+  boxShadow: "0 0px 20px rgba(0, 0, 0, 0.5)",
+  transform: "rotate(-10deg)",
+  marginTop: "-80px",
+  cursor: "pointer",
+  transition: "transform 0.3s ease",
+};
+
+const sectionStyle = {
+  width: "50%",
+  padding: "20px",
+  backgroundColor: "#F7F7F7",
+  color: "#333",
+  borderRadius: "20px",
 };
 
 const footerStyle = {
   marginTop: "auto",
   padding: "20px",
-  backgroundColor: "#29347A",
+  backgroundColor: "#1c1f1f",
   color: "#fff",
 };
 
 const LandingPage = () => {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [isSecHovered, setIsSecHovered] = useState(false);
+
   return (
     <div>
       <div style={landingPageStyle}>
-        <header style={headerStyle}>
-          <h1>Welcome to My Coding Website</h1>
-        </header>
-        {/* <section>
-          <p>
-            Start your coding journey with our interactive learning platform.
-          </p>
-          <p>Explore various coding stacks and enhance your skills.</p>
-        </section> */}
+        <div>
+          <img
+            style={{
+              width: "20vw",
+              height: "40vh",
+              transform: "rotate(10deg)",
+            }}
+            src={PipCImage}
+          />
+        </div>
+        <div style={{ marginRight: "330px" }}>
+          <header style={headerStyle}>
+            <h1>Welcome to PipC</h1>
+          </header>
+          <section>
+            <p style={{ fontFamily: "system-ui" }}>
+              <span
+                style={{
+                  fontWeight: 900,
+                  color: "#636363",
+                  fontFamily: "cursive",
+                  fontSize: "20px",
+                }}
+              >
+                Unlock the World of Coding Excellence:{" "}
+              </span>
+              <spin
+                style={{
+                  fontSize: "25px",
+                  fontFamily: "Cedarville Cursive",
+                  fontWeight: "900",
+                  color: "#3c847f",
+                  textDecoration: "underline",
+                }}
+              >
+                Learn, Build, and Master Programming
+              </spin>{" "}
+            </p>
+          </section>
+        </div>
       </div>
       <div style={sectionContainerStyle}>
         <div style={sectionStyle}>
-          <h2>Become the 10x Developer</h2>
-          <p>This is the content of the first additional section.</p>
+          <Stack direction="row" spacing={2}>
+            <DemoPaper variant="">
+              <Typography
+                sx={{
+                  paddingBottom: 4,
+                  fontFamily: "cursive",
+                  textDecoration: "overline red",
+                }}
+                variant="h4"
+              >
+                Learn, Code, Excel
+              </Typography>
+              <Typography
+                sx={{
+                  fontFamily: "Cursive",
+                  fontWeight: 500,
+                  color: "#3C847F",
+                }}
+                variant="h6"
+              >
+                Welcome to the ultimate destination for mastering coding and
+                software development. Whether you're a beginner or an
+                experienced programmer, our platform is designed to empower you
+                on your coding journey. Explore, learn, and build with
+                confidence.
+              </Typography>
+            </DemoPaper>
+          </Stack>
         </div>
         <div style={sectionStyle}>
-          
           <img
             src={SecondSectionImage}
             alt="Additional Section Image"
-            style={imgStyle}
+            style={{
+              ...imgStyle,
+              transform: isHovered ? "rotate(0deg)" : imgStyle.transform,
+            }}
+            onMouseOver={() => setIsHovered(true)}
+            onMouseOut={() => setIsHovered(false)}
+          />
+        </div>
+        <div>
+          <img
+            style={{
+              width: "20vw",
+              height: "40vh",
+              boxShadow: "0 0px 20px rgba(0, 0, 0, 0.5)",
+              cursor: 'pointer',
+              transition: "transform 0.3s ease",
+              transform: isSecHovered ? "rotate(0deg)" : "rotate(10deg)",
+            }}
+            src={QuoteImage}
+            onMouseOver={() => setIsSecHovered(true)}
+            onMouseOut={() => setIsSecHovered(false)}
           />
         </div>
       </div>
-      <Link to="/courses">
-        <button style={buttonStyle}>Explore Courses</button>
-      </Link>
       <div>
         <SimpleContainer />
       </div>
